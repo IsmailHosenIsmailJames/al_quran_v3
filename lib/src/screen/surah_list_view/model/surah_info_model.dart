3620949a757ea40880c2df98d5ce21ab6d2bf386
Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-04-04 08:45:56 +0600

    [fit] surah list view

---
+++ lib/src/screen/surah_list_view/model/surah_info_model.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+class SurahInfoModel {
+  final int id;
+  final int name;
+  final String nameSimple;
+  final String nameArabic;
+  final int revelationOrder;
+  final String revelationPlace;
+  final int versesCount;
+  final String pagesRange;
+
+  SurahInfoModel({
+    required this.id,
+    required this.name,
+    required this.nameArabic,
+    required this.nameSimple,
+    required this.revelationOrder,
+    required this.revelationPlace,
+    required this.versesCount,
+    required this.pagesRange,
+  });
+
+  SurahInfoModel copyWith({
+    int? id,
+    int? name,
+    String? nameSimple,
+    String? nameArabic,
+    int? revelationOrder,
+    String? revelationPlace,
+    int? versesCount,
+    String? pagesRange,
+  }) => SurahInfoModel(
+    id: id ?? this.id,
+    name: name ?? this.name,
+    nameSimple: nameSimple ?? this.nameSimple,
+    revelationOrder: revelationOrder ?? this.revelationOrder,
+    revelationPlace: revelationPlace ?? this.revelationPlace,
+    versesCount: versesCount ?? this.versesCount,
+    pagesRange: pagesRange ?? this.pagesRange,
+    nameArabic: nameArabic ?? this.nameArabic,
+  );
+
+  factory SurahInfoModel.fromJson(String str) =>
+      SurahInfoModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory SurahInfoModel.fromMap(Map<String, dynamic> json) => SurahInfoModel(
+    id: json['id'],
+    name: json['name'],
+    nameSimple: json['name_simple'],
+    revelationOrder: json['revelation_order'],
+    revelationPlace: json['revelation_place'],
+    versesCount: json['verses_count'],
+    pagesRange: json['pages_range'],
+    nameArabic: json['name_arabic'],
+  );
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'name': name,
+    'name_simple': nameSimple,
+    'revelation_order': revelationOrder,
+    'revelation_place': revelationPlace,
+    'verses_count': versesCount,
+    'pages_range': pagesRange,
+    'name_arabic': nameArabic,
+  };
+}
