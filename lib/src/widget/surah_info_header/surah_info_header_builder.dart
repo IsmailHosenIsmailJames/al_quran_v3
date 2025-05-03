761aaf2a8a0e0bcf660372d3147b70d6a697af64
Ismail Hossain James <md.ismailhosenismailjames@gmail.com>
2025-05-03 14:23:19 +0600

    [fix] translation and tafsir meta data in surah_info_header

--- lib/src/widget/surah_info_header/surah_info_header_builder.dart
+++ lib/src/widget/surah_info_header/surah_info_header_builder.dart
@@ -5,8 +5,10 @@ import 'package:al_quran_v3/src/resources/meta_data/quran_ayah_count.dart';
 import 'package:al_quran_v3/src/screen/surah_list_view/model/surah_info_model.dart';
 import 'package:al_quran_v3/src/theme/colors/app_colors.dart';
 import 'package:al_quran_v3/src/theme/values/values.dart';
+import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class SurahInfoHeaderBuilder extends StatelessWidget {
   final SurahInfoModel surahInfoModel;
@@ -14,6 +16,8 @@ class SurahInfoHeaderBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Map translationMeta = Hive.box('quran_translation').get('meta_data');
+    final Map tafsirMeta = Hive.box('quran_tafsir').get('meta_data');
     return Container(
       margin: const EdgeInsets.only(left: 5, top: 5, bottom: 5, right: 10),
       decoration: BoxDecoration(
@@ -48,9 +52,29 @@ class SurahInfoHeaderBuilder extends StatelessWidget {
                     '${surahInfoModel.id}. ${surahInfoModel.nameSimple} ( ${surahInfoModel.nameArabic} )',
                     style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  Text('Verse Count: ${surahInfoModel.versesCount}'),
-                  const Text('Translation: '),
-                  const Text('Tafsir: '),
+                  Row(
+                    children: [
+                      const Text('Verse Count: '),
+                      Text(
+                        surahInfoModel.versesCount.toString(),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width - 120 - 30,
+                    child: Text(
+                      'Translation: ${translationMeta['name'].toString().split('/').last.replaceAll('.simple', '').replaceAll('.json.txt', '').replaceAll('_', ' ').capitalize()}',
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width - 120 - 30,
+                    child: Text(
+                      'Tafsir: ${tafsirMeta['name'].toString().split('/').last.replaceAll('.json.txt', '').replaceAll('_', ' ').capitalize()}',
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ),
                   SizedBox(
                     height: 25,
                     child: TextButton(
@@ -74,31 +98,28 @@ class SurahInfoHeaderBuilder extends StatelessWidget {
           ),
           Align(
             alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: IconButton(
-                style: IconButton.styleFrom(
-                  padding: EdgeInsets.zero,
-                  backgroundColor: AppColors.primaryColor,
-                  foregroundColor: Colors.white,
-                ),
-                onPressed: () {
-                  String startAyahKey = '${surahInfoModel.id}:1';
-                  String endAyahKey = getEndAyahKeyFromSurahNumber(
-                    surahInfoModel.id,
-                  );
-
-                  log(startAyahKey, name: 'Start Ayah Key');
-                  log(endAyahKey, name: 'End Ayah Key');
-
-                  AudioPlayerManager.playMultipleAyahAsPlaylist(
-                    context: context,
-                    startAyahKey: startAyahKey,
-                    endAyahKey: endAyahKey,
-                  );
-                },
-                icon: const Icon(Icons.play_arrow_rounded),
+            child: IconButton(
+              style: IconButton.styleFrom(
+                padding: EdgeInsets.zero,
+                backgroundColor: AppColors.primaryColor,
+                foregroundColor: Colors.white,
               ),
+              onPressed: () {
+                String startAyahKey = '${surahInfoModel.id}:1';
+                String endAyahKey = getEndAyahKeyFromSurahNumber(
+                  surahInfoModel.id,
+                );
+
+                log(startAyahKey, name: 'Start Ayah Key');
+                log(endAyahKey, name: 'End Ayah Key');
+
+                AudioPlayerManager.playMultipleAyahAsPlaylist(
+                  context: context,
+                  startAyahKey: startAyahKey,
+                  endAyahKey: endAyahKey,
+                );
+              },
+              icon: const Icon(Icons.play_arrow_rounded),
             ),
           ),
         ],
