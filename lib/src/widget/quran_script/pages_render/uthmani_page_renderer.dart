6919863b65faea0fa7ce5869392106239f3d4aba
Ismail Hossain James <md.ismailhosenismailjames@gmail.com>
2025-05-04 23:54:32 +0600

    [fit] indopak_page_renderer & uthmani_page_renderer added

--- lib/src/widget/quran_script/pages_render/uthmani_page_renderer.dart
+++ lib/src/widget/quran_script/pages_render/uthmani_page_renderer.dart
@@ -1,3 +1,4 @@
+import 'package:al_quran_v3/main.dart';
 import 'package:flutter/material.dart';
 
 class UthmaniPageRenderer extends StatelessWidget {
@@ -12,6 +13,27 @@ class UthmaniPageRenderer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    String quranScript = '';
+    for (String ayahKey in ayahsKey) {
+      List words =
+          uthmaniScript[ayahKey.split(':').first]?[ayahKey.split(':').last] ??
+          [];
+      for (final word in words) {
+        quranScript += '$word ';
+      }
+    }
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Text(
+        quranScript,
+        style: TextStyle(
+          fontSize: baseTextStyle?.fontSize ?? 24,
+          fontFamily: baseTextStyle?.fontFamily ?? 'QPC_Hafs',
+          fontWeight: baseTextStyle?.fontWeight,
+        ),
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.rtl,
+      ),
+    );
   }
 }
