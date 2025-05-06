5c184f2be0e2c1e3251dcb47beca90a7e6d03914
Ismail Hossain James <md.ismailhosenismailjames@gmail.com>
2025-05-06 16:20:05 +0600

    [fit] all kind of ayah or surah move able

--- lib/src/screen/surah_info/surah_info_view.dart
+++ lib/src/screen/surah_info/surah_info_view.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 
 import 'package:al_quran_v3/src/screen/quran_script_view/quran_script_view.dart';
 import 'package:al_quran_v3/src/screen/surah_list_view/model/surah_info_model.dart';
+import 'package:al_quran_v3/src/widget/surah_info_header/surah_info_header_builder.dart';
+import 'package:dartx/dartx_io.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 
@@ -29,11 +31,33 @@ class SurahInfoView extends StatelessWidget {
             log(url.toString());
             try {
               url ??= '';
-              if (url.startsWith('/') && url.contains('-')) {
+              int countOfSlash = url.characters.count(
+                (element) => element == '/',
+              );
+              int countOfDash = url.characters.count(
+                (element) => element == '-',
+              );
+              log(countOfSlash.toString());
+              log(countOfDash.toString());
+              if (countOfSlash == 1) {
+                int? surahNumber = int.tryParse(url.split('/').last);
+                if (surahNumber != null) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => QuranScriptView(
+                            startKey: '$surahNumber:${1}',
+                            endKey: getEndAyahKeyFromSurahNumber(surahNumber),
+                          ),
+                    ),
+                  );
+                }
+              } else if (countOfSlash == 2) {
                 String surahNumber = url.split('/')[1];
                 List<String> ayahsRange = url.split('/').last.split('-');
-                String startAyahKey = '$surahNumber:${ayahsRange[0]}';
-                String endAyahKey = '$surahNumber:${ayahsRange[1]}';
+                String startAyahKey = '$surahNumber:${ayahsRange.first}';
+                String endAyahKey = '$surahNumber:${ayahsRange.last}';
                 Navigator.push(
                   context,
                   MaterialPageRoute(
