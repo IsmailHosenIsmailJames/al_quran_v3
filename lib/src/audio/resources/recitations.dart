7ea7531538dff9c231ab066bf1fbd4508525ca56
MD. Ismail Hossain James <md.ismailhosenismailjames@gmail.com>
2025-05-27 15:41:47 +0600

    [refactor] recitation data source refactor
    The recitations data from `quran_com` and `every_ayah_com` have been consolidated into a single `recitations.dart` file. Each entry in the new list now includes a "source" field to indicate its origin (either "EveryAyah.com" or "Quran.com"). Some redundant or lower quality bitrates from `every_ayah_com` have been removed to streamline the list.

--- lib/src/audio/resources/recitations.dart
+++ lib/src/audio/resources/recitations.dart
@@ -1,324 +1,288 @@
-List<Map<String, dynamic>> recitationsInfoListEveryAyahCom = [
+List<Map<String, dynamic>> recitationsInfoList = [
   {
     "link": "https://everyayah.com/data/Abdul_Basit_Murattal_64kbps",
     "name": "Abdul Basit Murattal",
-  },
-  {
-    "link": "https://everyayah.com/data/Abdul_Basit_Murattal_192kbps",
-    "name": "Abdul Basit Murattal",
-  },
-  {
-    "link": "https://everyayah.com/data/Abdul_Basit_Mujawwad_128kbps",
-    "name": "Abdul Basit Mujawwad",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Abdullah_Basfar_32kbps",
     "name": "Abdullah Basfar",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Abdullah_Basfar_64kbps",
     "name": "Abdullah Basfar",
-  },
-  {
-    "link": "https://everyayah.com/data/Abdullah_Basfar_192kbps",
-    "name": "Abdullah Basfar",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Abdurrahmaan_As-Sudais_64kbps",
     "name": "Abdurrahmaan As-Sudais",
+    "source": "EveryAyah.com",
   },
-  {
-    "link": "https://everyayah.com/data/Abdurrahmaan_As-Sudais_192kbps",
-    "name": "Abdurrahmaan As-Sudais",
-  },
+
   {
     "link": "https://everyayah.com/data/AbdulSamad_64kbps_QuranExplorer.Com",
-    "name": "AbdulSamad QuranExplorer.Com",
+    "name": "Abdul Samad",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Abu_Bakr_Ash-Shaatree_64kbps",
     "name": "Abu Bakr Ash-Shaatree",
+    "source": "EveryAyah.com",
   },
-  {
-    "link": "https://everyayah.com/data/Abu_Bakr_Ash-Shaatree_128kbps",
-    "name": "Abu Bakr Ash-Shaatree",
-  },
+
   {
     "link":
         "https://everyayah.com/data/Ahmed_ibn_Ali_al-Ajamy_64kbps_QuranExplorer.Com",
     "name": "Ahmed ibn Ali al-Ajamy QuranExplorer.Com",
-  },
-  {
-    "link":
-        "https://everyayah.com/data/Ahmed_ibn_Ali_al-Ajamy_128kbps_ketaballah.net",
-    "name": "Ahmed ibn Ali al-Ajamy KetabAllah.Net",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Alafasy_64kbps",
     "name": "Alafasy",
-  },
-  {
-    "link": "https://everyayah.com/data/Alafasy_128kbps",
-    "name": "Alafasy",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Ghamadi_40kbps",
     "name": "Ghamadi",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Hani_Rifai_64kbps",
     "name": "Hani Rifai",
-  },
-  {
-    "link": "https://everyayah.com/data/Hani_Rifai_192kbps",
-    "name": "Hani Rifai",
-  },
-  {
-    "link": "https://everyayah.com/data/Husary_64kbps",
-    "name": "Husary",
-  },
-  {
-    "link": "https://everyayah.com/data/Husary_128kbps",
-    "name": "Husary",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Husary_Mujawwad_64kbps",
     "name": "Husary Mujawwad",
-  },
-  {
-    "link": "https://everyayah.com/data/Husary_128kbps_Mujawwad",
-    "name": "Husary Mujawwad",
-  },
-  {
-    "link": "https://everyayah.com/data/Hudhaify_32kbps",
-    "name": "Hudhaify",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Hudhaify_64kbps",
     "name": "Hudhaify",
-  },
-  {
-    "link": "https://everyayah.com/data/Hudhaify_128kbps",
-    "name": "Hudhaify",
-  },
-  {
-    "link": "https://everyayah.com/data/Ibrahim_Akhdar_32kbps",
-    "name": "Ibrahim Akhdar",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Ibrahim_Akhdar_64kbps",
     "name": "Ibrahim Akhdar",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Maher_AlMuaiqly_64kbps",
     "name": "Maher Al Muaiqly",
-  },
-  {
-    "link": "https://everyayah.com/data/MaherAlMuaiqly128kbps",
-    "name": "Maher Al Muaiqly",
-  },
-  {
-    "link": "https://everyayah.com/data/Menshawi_16kbps",
-    "name": "Menshawi",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Menshawi_32kbps",
     "name": "Menshawi",
-  },
-  {
-    "link": "https://everyayah.com/data/Minshawy_Mujawwad_64kbps",
-    "name": "Minshawy Mujawwad",
-  },
-  {
-    "link": "https://everyayah.com/data/Minshawy_Mujawwad_192kbps",
-    "name": "Minshawy Mujawwad",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Minshawy_Murattal_128kbps",
     "name": "Minshawy Murattal",
+    "source": "EveryAyah.com",
   },
-  {
-    "link": "https://everyayah.com/data/Mohammad_al_Tablaway_64kbps",
-    "name": "Mohammad al Tablaway",
-  },
-  {
-    "link": "https://everyayah.com/data/Mohammad_al_Tablaway_128kbps",
-    "name": "Mohammad al Tablaway",
-  },
-  {
-    "link": "https://everyayah.com/data/Muhammad_Ayyoub_128kbps",
-    "name": "Muhammad Ayyoub",
-  },
+
   {
     "link": "https://everyayah.com/data/Muhammad_Ayyoub_64kbps",
     "name": "Muhammad Ayyoub",
-  },
-  {
-    "link": "https://everyayah.com/data/Muhammad_Ayyoub_32kbps",
-    "name": "Muhammad Ayyoub",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Muhammad_Jibreel_64kbps",
     "name": "Muhammad Jibreel",
+    "source": "EveryAyah.com",
   },
-  {
-    "link": "https://everyayah.com/data/Muhammad_Jibreel_128kbps",
-    "name": "Muhammad Jibreel",
-  },
+
   {
     "link": "https://everyayah.com/data/Mustafa_Ismail_48kbps",
     "name": "Mustafa Ismail",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Saood_ash-Shuraym_64kbps",
     "name": "Saood bin Ibraaheem Ash-Shuraym",
-  },
-  {
-    "link": "https://everyayah.com/data/Saood_ash-Shuraym_128kbps",
-    "name": "Saood bin Ibraaheem Ash-Shuraym",
-  },
-  {
-    "link":
-        "https://everyayah.com/data/English/Sahih_Intnl_Ibrahim_Walk_192kbps",
-    "name":
-        "(English) Translated by Sahih International Recited by Ibrahim Walk",
-  },
-  {
-    "link": "https://everyayah.com/data/MultiLanguage/Basfar_Walk_192kbps",
-    "name": "MultiLanguage/Basfar Walk",
-  },
-  {
-    "link": "https://everyayah.com/data/translations/Makarem_Kabiri_16Kbps",
-    "name": "(Persian) Translated by Makarem Recited by Kabiri",
-  },
-  {
-    "link":
-        "https://everyayah.com/data/translations/Fooladvand_Hedayatfar_40Kbps",
-    "name": "(Persian) Translated by Fooladvand Recited by Hedayatfar",
-  },
-  {
-    "link": "https://everyayah.com/data/Parhizgar_48kbps",
-    "name": "Parhizgar_64Kbps",
-  },
-  {
-    "link": "https://everyayah.com/data/translations/azerbaijani/balayev",
-    "name": "Balayev",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Salaah_AbdulRahman_Bukhatir_128kbps",
     "name": "Salaah AbdulRahman Bukhatir",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Muhsin_Al_Qasim_192kbps",
     "name": "Muhsin Al Qasim",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Abdullaah_3awwaad_Al-Juhaynee_128kbps",
     "name": "Abdullaah 3awwaad Al-Juhaynee",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Salah_Al_Budair_128kbps",
     "name": "Salah Al Budair",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Abdullah_Matroud_128kbps",
     "name": "Abdullah Matroud",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Ahmed_Neana_128kbps",
     "name": "Ahmed Neana",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Muhammad_AbdulKareem_128kbps",
     "name": "Muhammad AbdulKareem",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/khalefa_al_tunaiji_64kbps",
     "name": "Khalefa Al-Tunaiji",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/mahmoud_ali_al_banna_32kbps",
     "name": "Mahmoud Ali Al-Banna",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/warsh/warsh_ibrahim_aldosary_128kbps",
     "name": "(Warsh) Ibrahim Al-Dosary",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/warsh/warsh_yassin_al_jazaery_64kbps",
     "name": "(Warsh) Yassin Al-Jazaery",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/warsh/warsh_Abdul_Basit_128kbps",
     "name": "(Warsh) Abdul Basit",
-  },
-  {
-    "link":
-        "https://everyayah.com/data/translations/urdu_shamshad_ali_khan_46kbps",
-    "name": "(Urdu) Shamshad Ali Khan",
-  },
-  {
-    "link": "https://everyayah.com/data/Karim_Mansoori_40kbps",
-    "name": "Karim Mansoori (Iran)",
-  },
-  {
-    "link": "https://everyayah.com/data/Husary_Muallim_128kbps",
-    "name": "Husary (Muallim)",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Khaalid_Abdullaah_al-Qahtaanee_192kbps",
     "name": "Khalid Abdullah al-Qahtanee",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Yasser_Ad-Dussary_128kbps",
     "name": "Yasser_Ad-Dussary",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Nasser_Alqatami_128kbps",
     "name": "Nasser_Alqatami",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Ali_Hajjaj_AlSuesy_128kbps",
     "name": "Ali_Hajjaj_AlSuesy",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Sahl_Yassin_128kbps",
     "name": "Sahl_Yassin",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/ahmed_ibn_ali_al_ajamy_128kbps",
     "name": "Ahmed Ibn Ali Al Ajamy",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/translations/besim_korkut_ajet_po_ajet",
     "name": "Besim Korkut (Bosnian)",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/aziz_alili_128kbps",
     "name": "Aziz Alili",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Yaser_Salamah_128kbps",
     "name": "Yaser Salamah",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Akram_AlAlaqimy_128kbps",
     "name": "Akram Al Alaqimy",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Ali_Jaber_64kbps",
     "name": "Ali Jaber",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Fares_Abbad_64kbps",
     "name": "Fares Abbad",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/translations/urdu_farhat_hashmi",
     "name": "Farhat Hashmi (Urdu word for word translation)",
+    "source": "EveryAyah.com",
   },
   {
     "link": "https://everyayah.com/data/Ayman_Sowaid_64kbps",
     "name": "Ayman Sowaid",
-  }
+    "source": "EveryAyah.com",
+  },
+  {
+    "link": "https://verses.quran.foundation/AbdulBaset/Mujawwad/mp3",
+    "name": "Abdul Baset AbdulSamad",
+    "source": "Quran.com",
+  },
+  {
+    "link": "https://verses.quran.foundation/Sudais/mp3",
+    "name": "Abdur-Rahman as-Sudais",
+    "source": "Quran.com",
+  },
+  {
+    "link": "https://verses.quran.foundation/Shatri/mp3",
+    "name": "Abu Bakr al-Shatri",
+    "source": "Quran.com",
+  },
+  {
+    "link": "https://verses.quran.foundation/Rifai/mp3",
+    "name": "Hani ar-Rifai",
+    "source": "Quran.com",
+  },
+  {
+    "link": "https://mirrors.quranicaudio.com/everyayah/Husary_64kbps",
+    "name": "Mahmoud Khalil Al-Husary",
+    "source": "Quran.com",
+  },
+  {
+    "link": "https://verses.quran.foundation/Alafasy/mp3",
+    "name": "Mishari Rashid al-`Afasy",
+    "source": "Quran.com",
+  },
+  {
+    "link": "https://verses.quran.foundation/Murattal/mp3",
+    "name": "Muhammad al-Murattal",
+    "source": "Quran.com",
+  },
+  {
+    "link": "https://verses.quran.foundation/Minshawi/Mujawwad/mp3",
+    "name": "Mohamed Siddiq al-Minshawi",
+    "source": "Quran.com",
+  },
+  {
+    "link": "https://verses.quran.foundation/Shuraym/mp3",
+    "name": "Sa`ud ash-Shuraym",
+    "source": "Quran.com",
+  },
+  {
+    "link":
+        "https://mirrors.quranicaudio.com/everyayah/Mohammad_al_Tablaway_128kbps/",
+    "name": "Mohamed al-Tablawi",
+    "source": "Quran.com",
+  },
 ];
