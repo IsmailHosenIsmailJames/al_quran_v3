14e3aa089318cb4aa2a0a493ee080f88e5e26404
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-06-03 22:21:21 +0600

    [fit] UI enhancement

--- lib/src/theme/colors/app_colors.dart
+++ lib/src/theme/colors/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   static Color primary = const Color(0xff1E9579);
   static Color primaryShade100 = primary.withValues(alpha: 0.1);
   static Color primaryShade200 = primary.withValues(alpha: 0.2);
+  static Color primaryShade300 = primary.withValues(alpha: 0.3);
   static Color secondary = Colors.orange;
   static Color mutedGray = Colors.grey.withValues(alpha: 0.2);
 }
