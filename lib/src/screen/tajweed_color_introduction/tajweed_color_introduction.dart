07f75ac324c3b7638f261b5057d8a5ef467e4c61
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-07-06 18:32:57 +0600

    [fit] tajweed_color_introduction

---
+++ lib/src/screen/tajweed_color_introduction/tajweed_color_introduction.dart
@@ -0,0 +1,16 @@
+import "package:flutter/widgets.dart";
+
+class TajweedColorIntroduction extends StatefulWidget {
+  const TajweedColorIntroduction({super.key});
+
+  @override
+  State<TajweedColorIntroduction> createState() =>
+      _TajweedColorIntroductionState();
+}
+
+class _TajweedColorIntroductionState extends State<TajweedColorIntroduction> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
