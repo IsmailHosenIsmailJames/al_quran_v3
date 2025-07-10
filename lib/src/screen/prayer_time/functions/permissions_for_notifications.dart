f0dc127feda12df3b8b4efbbfafc39cde0f468f7
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-07-10 21:55:37 +0600

    [fix] bug fix for iOS

--- lib/src/screen/prayer_time/functions/permissions_for_notifications.dart
+++ lib/src/screen/prayer_time/functions/permissions_for_notifications.dart
@@ -1,4 +1,5 @@
 import "dart:developer";
+import "dart:io";
 
 import "package:awesome_notifications/awesome_notifications.dart";
 import "package:permission_handler/permission_handler.dart";
@@ -9,15 +10,19 @@ Future<bool> requestPermissionForReminder() async {
   if (isPermissionGranted) {
     return true;
   }
-  PermissionStatus statusNotification = await Permission.notification.request();
-  PermissionStatus statusAlarm = await Permission.scheduleExactAlarm.request();
+
   bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
   if (!isAllowed) {
     isAllowed =
         await AwesomeNotifications().requestPermissionToSendNotifications();
   }
 
-  if (statusNotification.isGranted && statusAlarm.isGranted && isAllowed) {
+  PermissionStatus statusAlarm = PermissionStatus.granted;
+  if (Platform.isAndroid) {
+    statusAlarm = await Permission.scheduleExactAlarm.request();
+  }
+
+  if (isAllowed && statusAlarm.isGranted) {
     log("All Permission is Granted", name: "Permission");
     isPermissionGranted = true;
     return true;
