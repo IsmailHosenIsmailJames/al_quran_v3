660003c88ce03c8b7571ccb52fd016e5b524f582
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-07-25 20:14:50 +0600

    [fit] scroll with audio recitation bug fixed

--- lib/src/audio/model/ayahkey_management.dart
+++ lib/src/audio/model/ayahkey_management.dart
@@ -3,12 +3,14 @@ class AyahKeyManagement {
   final String end;
   final String current;
   final List<String> ayahList;
+  final int? lastScrolledPageNumber;
 
   AyahKeyManagement({
     required this.start,
     required this.end,
     required this.current,
     required this.ayahList,
+    this.lastScrolledPageNumber,
   });
 
   AyahKeyManagement copyWith({
@@ -16,12 +18,15 @@ class AyahKeyManagement {
     String? end,
     String? current,
     List<String>? ayahList,
+    int? lastScrolledPageNumber,
   }) {
     return AyahKeyManagement(
       start: start ?? this.start,
       end: end ?? this.end,
       current: current ?? this.current,
       ayahList: ayahList ?? this.ayahList,
+      lastScrolledPageNumber:
+          lastScrolledPageNumber ?? this.lastScrolledPageNumber,
     );
   }
 }
