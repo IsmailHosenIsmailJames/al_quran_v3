3b0365d6dbab98fbd1f2d67110adfb3899d93dcc
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-08-03 11:23:58 +0600

    [fix] Fix Surah List View Navigation

--- lib/src/functions/quran_ayahs_function/get_page_number.dart
+++ lib/src/functions/quran_ayahs_function/get_page_number.dart
@@ -2,7 +2,10 @@ import "package:al_quran_v3/src/functions/basic_functions.dart";
 import "package:al_quran_v3/src/resources/quran_resources/quran_pages_info.dart";
 
 int? getPageNumber(String ayahKey) {
-  int ayahID = convertKeyToAyahNumber(ayahKey)!;
+  int? ayahID = convertKeyToAyahNumber(ayahKey);
+  if (ayahID == null) {
+    return null;
+  }
   for (Map info in quranPagesInfo) {
     if (info["s"] <= ayahID && info["e"] >= ayahID) {
       return info["i"] + 1;
