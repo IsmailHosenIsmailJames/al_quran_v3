0286509a205f74abc94c50310427b828013584bb
MD. Ismail Hosen <md.ismailhosenismailjames@gmail.com>
2025-08-22 07:25:20 +0600

    [fix] Update dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,62 +3,62 @@ import java.io.FileInputStream
 
 
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
+  id("com.android.application")
+  id("kotlin-android")
+  id("dev.flutter.flutter-gradle-plugin")
 }
 
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+  keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
-    namespace = "com.ismail_hosen.quran_audio"
-    compileSdk = 36
-    ndkVersion = "27.0.12077973"
+  namespace = "com.ismail_hosen.quran_audio"
+  compileSdk = 36
+  ndkVersion = "27.0.12077973"
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_11.toString()
+  }
+
+  defaultConfig {
+    applicationId = "com.ismail_hosen.quran_audio"
+    minSdkVersion(flutter.minSdkVersion)
+    targetSdk = flutter.targetSdkVersion
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
+    multiDexEnabled = true
+  }
+
+  packagingOptions {
+    jniLibs {
+      useLegacyPackaging = true
     }
+  }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+  signingConfigs {
+    create("release") {
+      keyAlias = keystoreProperties["keyAlias"] as String
+      keyPassword = keystoreProperties["keyPassword"] as String
+      storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+      storePassword = keystoreProperties["storePassword"] as String
     }
+  }
 
-    defaultConfig {
-        applicationId = "com.ismail_hosen.quran_audio"
-        minSdk = 21
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-        multiDexEnabled = true
+  buildTypes {
+    release {
+      signingConfig = signingConfigs.getByName("release")
     }
-
-     packagingOptions {
-        jniLibs {
-            useLegacyPackaging = true
-        }
-     }
-
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-        }
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
+  }
 }
 
 flutter {
-    source = "../.."
+  source = "../.."
 }
