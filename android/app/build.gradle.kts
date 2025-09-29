e4594d40f3744a0fefb540d371a93df7d313e0a5
Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-09-29 20:46:05 +0600

    [feat] changed application id to make difference between main and dev

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ismail_hosen_james.al_bayan_quran"
+        applicationId = "com.ismail_hosen_james.al_bayan_quran.dev"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdkVersion(flutter.minSdkVersion)
