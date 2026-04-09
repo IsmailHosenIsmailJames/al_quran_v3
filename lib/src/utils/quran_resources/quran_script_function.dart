0f7c3c614740679678f9f0a5d648c3767dad4ddf
Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2026-04-09 18:04:03 +0600

    feat: add warshScript parameter to getWordListOfAyah and implement character swapping logic for non-Warsh scripts

--- lib/src/utils/quran_resources/quran_script_function.dart
+++ lib/src/utils/quran_resources/quran_script_function.dart
@@ -2,6 +2,7 @@ import "dart:convert";
 
 import "package:al_quran_v3/src/utils/tajweed_rules.dart";
 import "package:al_quran_v3/src/widget/quran_script/model/script_info.dart";
+import "package:dartx/dartx.dart";
 import "package:flutter/services.dart";
 
 class QuranScriptFunction {
@@ -31,14 +32,24 @@ class QuranScriptFunction {
   static List<String> getWordListOfAyah(
     QuranScriptType type,
     String surah,
-    String ayah,
-  ) {
+    String ayah, {
+    bool warshScript = false,
+  }) {
     final ayahData = List<String>.from(quranScriptMap[surah][ayah]);
     for (int i = 0; i < ayahData.length; i++) {
       for (int j = tajweedRulesList.length - 1; 0 <= j; j--) {
         ayahData[i] = ayahData[i].replaceAll("r$j", tajweedRulesList[j]);
       }
     }
+
+    if (!warshScript) {
+      for (int i = 0; i < ayahData.length; i++) {
+        ayahData[i] = ayahData[i].replaceAll(1761.toChar(), "Backup");
+        ayahData[i] = ayahData[i].replaceAll(1618.toChar(), 1761.toChar());
+        ayahData[i] = ayahData[i].replaceAll("Backup", 1618.toChar());
+      }
+    }
+
     return ayahData;
   }
 }
