967bf0042bade8e354e8d35a87106bffa3295e80
MD. Ismail Hossain <md.ismailhosenismailjames@gmail.com>
2026-05-19 19:38:06 +0600

    feat: add word-by-word initialization and open collection boxes during app reset

--- lib/src/utils/reset_app.dart
+++ lib/src/utils/reset_app.dart
@@ -1,8 +1,10 @@
+import "package:al_quran_v3/src/screen/collections/collection_page.dart";
 import "package:al_quran_v3/src/screen/setup/cubit/resources_progress_cubit_cubit.dart";
 import "package:al_quran_v3/src/screen/setup/setup_page.dart";
 import "package:al_quran_v3/src/utils/quran_resources/quran_tafsir_function.dart";
 import "package:al_quran_v3/src/utils/quran_resources/quran_translation_function.dart";
 import "package:al_quran_v3/src/utils/quran_resources/segmented_resources_manager.dart";
+import "package:al_quran_v3/src/utils/quran_resources/word_by_word_function.dart";
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 import "package:hive_ce_flutter/hive_flutter.dart";
@@ -17,9 +19,15 @@ Future<void> resetTheApp(BuildContext context) async {
 
   await Hive.deleteFromDisk();
   await Hive.openBox("user");
+
   await QuranTranslationFunction.init();
   await QuranTafsirFunction.init();
   await SegmentedResourcesManager.init();
+  WordByWordFunction.init();
+  Hive.openBox(CollectionType.notes.name);
+  Hive.openBox(CollectionType.pinned.name);
+  Hive.openBox(CollectionType.bookmark.name);
+  SegmentedResourcesManager.init();
 
   Navigator.pushAndRemoveUntil(
     context,
